748fcebbb7d4dff7c7b4a83360b2361c93afd1ac
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2013-05-10 01:07:48 -0500

    oops

---
+++ config.h
@@ -0,0 +1,35 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `GL' library (-lGL). */
+#define HAVE_LIBGL 1
+
+/* Define to 1 if you have the `GLU' library (-lGLU). */
+#define HAVE_LIBGLU 1
+
+/* Define to 1 if you have the `glut' library (-lglut). */
+#define HAVE_LIBGLUT 1
+
+/* Name of package */
+#define PACKAGE "gluttest"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "c.mitch.carroll@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "gluttest"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "gluttest 1.6"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gluttest"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.6"
+
+/* Version number of package */
+#define VERSION "1.6"
